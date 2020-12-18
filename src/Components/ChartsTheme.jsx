64d3aa0e9cd90402cc61d93da7a977b1ae1932a4
Sumod Sivadas <contactsumod@gmail.com>
2020-12-18 12:44:15 +0530

    Charts theme

---
+++ src/Components/ChartsTheme.jsx
@@ -0,0 +1,82 @@
+window.FusionCharts.register("theme", {
+    name: "Dashboard",
+    theme: {
+      base: {
+        chart: {
+          bgAlpha: "0",
+          canvasBgAlpha: "0",
+          baseFont: "Menlo",
+          baseFontSize: "14",
+          baseFontBold: "0",
+          chartBottomMargin: "0",
+          chartTopMargin: "0",
+          chartLeftMargin: "0",
+          chartRightMargin: "0",
+          canvasBottomMargin: "0",
+          canvasTopMargin: "0",
+          canvasRightMargin: "0",
+          canvasLeftMargin: "0",
+          showBorder: "0",
+          showCanvasBorder: "0",
+          baseFontColor: "#ffffff",
+          captionFontBold: "0",
+          captionFontSize: "14",
+          subCaptionFontSize: "14",
+          tooltipColor: "#ffffff",
+          tooltipBgColor: "#000000",
+          tooltipBgAlpha: "60",
+          tooltipPadding: "5",
+          toolTipBorderAlpha: "10",
+          toolTipBorderRadius: "3",
+          showValues: "0",
+          legendBgAlpha: "0",
+          legendBorderAlpha: "0",
+          legendBorderThickness: "0"
+        }
+      },
+      bar2d: {
+        chart: {
+          placeValuesInside: "0",
+          usePlotGradientColor: "0",
+          showAlternateVGridColor: "0",
+          chartLeftMargin: "5",
+          canvasLeftMargin: "5",
+          divLineAlpha: "10",
+          divLineColor: "#ffffff",
+          captionFontColor: "#8091ab",
+          paletteColors: "#1D91C0",
+          valuePadding: "5",
+          yAxisName: "Orders",
+          yAxisNameAlpha: "50",
+          yAxisNameFontSize: "12",
+          yAxisNamePadding: "20",
+          valueFontBold: "0",
+          valueFontSize: "12",
+          plotToolText: "<div>$label<br><b>$value orders</b>",
+          captionAlignment: "left",
+          captionPadding: "20"
+        }
+      },
+      doughnut2d: {
+        chart: {
+          captionFontSize: "14",
+          captionFontColor: "#8091ab",
+          showLabels: "0",
+          showValues: "0",
+          use3DLighting: "0",
+          showPlotBorder: "0",
+          defaultCenterLabel: "75%",
+          pieRadius: "45",
+          doughnutRadius: "33",
+          showTooltip: "0",
+          enableRotation: "0",
+          enableSlicing: "0",
+          startingAngle: "90"
+        }
+      },
+      
+        }
+      }
+    }
+  });
+  
