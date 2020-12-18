d90f1a88a8beb25c40ef7ceedeab94e55c5baf66
Sumod Sivadas <contactsumod@gmail.com>
2020-12-18 22:19:26 +0530

    Title and Charts component added

---
+++ src/Components/Charts.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from 'recharts';
+import Title from './Title';
+
+// Generate Sales Data
+function createData(time, amount) {
+  return { time, amount };
+}
+
+const data = [
+  createData('0am', 0),
+  createData('3am', 300),
+  createData('6am', 600),
+  createData('9am', 800),
+  createData('12pm', 1500),
+  createData('3pm', 2000),
+  createData('6pm', 2400),
+  createData('9pm', 2400),
+  createData('12am', undefined),
+];
+
+export default function Chart() {
+  return (
+    <React.Fragment>
+      <Title>Sales Today</Title>
+      <ResponsiveContainer>
+        <LineChart
+          data={data}
+          margin={{
+            top: 16,
+            right: 16,
+            bottom: 0,
+            left: 24,
+          }}
+        >
+          <XAxis dataKey="time" />
+          <YAxis>
+            <Label angle={270} position="left" style={{ textAnchor: 'middle' }}>
+              Sales ($)
+            </Label>
+          </YAxis>
+          <Line type="monotone" dataKey="amount" stroke="#556CD6" dot={false} />
+        </LineChart>
+      </ResponsiveContainer>
+    </React.Fragment>
+  );
+}
---
+++ src/Components/Title.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    title: {
+        color: '#444545',
+        textAlign: 'left',
+    },
+}));
+
+const Title = (props) => {
+    const classes = useStyles();
+
+    return (
+        <Typography
+            className={classes.title}
+            component='h2'
+            variant='h6'
+            color='primary'
+            gutterBottom
+        >
+            {props.children}
+        </Typography>
+    );
+};
+
+Title.propTypes = {
+    children: PropTypes.node,
+};
+
+export default Title;
