7d5eae1c9af29e4fde6e05a68cd6f25828a2fbf1
Sumod Sivadas <contactsumod@gmail.com>
2020-12-19 17:46:00 +0530

    ListItems drawer elements

--- src/Components/ListItems.jsx
+++ src/Components/ListItems.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
+import SettingsIcon from '@material-ui/icons/Settings';
 import DashboardIcon from '@material-ui/icons/Dashboard';
+import Divider from '@material-ui/core/Divider';
 
-
+import Avatar from '@material-ui/core/Avatar';
+import { Link } from "react-router-dom";
 
 export const mainListItems = (
   <div>
+    <Avatar alt="photo" src="https://media.idownloadblog.com/wp-content/uploads/2018/01/Facebook-contact-icon.jpg" 
+        style={{margin: '0 auto' ,  width: '150px', height: '150px'}}/>
+        <br/>
   <p style={{margin: '0 auto' }}> Mr. FirstName LastName </p>
   
   <br/>
@@ -18,6 +24,14 @@ export const mainListItems = (
       </ListItemIcon>
       
       <Link to="/" style={{color: 'black' , textDecoration: 'none'}}>Dashboard</Link>
+    </ListItem>
+    
+    
+    <ListItem button>
+      <ListItemIcon>
+        <SettingsIcon />
+      </ListItemIcon>
+      <Link to="/settings" style={{color: 'black' , textDecoration: 'none'}}>Settings</Link>
     </ListItem>
   </div>
 );
