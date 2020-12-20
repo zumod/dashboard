614d8dfb9fd41478e91d984cbf72165698900c76
Sumod Sivadas <contactsumod@gmail.com>
2020-12-20 11:27:07 +0530

    Settings Component added

--- src/Components/Settings.jsx
+++ src/Components/Settings.jsx
@@ -2,13 +2,9 @@ import React from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import Drawer from './ResponsiveDrawer';
-import Chart from './Charts';
-import Deposits from './Appoinments';
-import Orders from './Orders';
+import Container from '@material-ui/core/Container';
+
 import { ThemeProvider } from '@material-ui/core/styles';
 import { createMuiTheme } from '@material-ui/core/styles';
 
@@ -30,22 +26,6 @@ const useStyles = makeStyles((theme) => ({
         justifyContent: 'flex-end',
         padding: '0 8px',
         ...theme.mixins.toolbar,
-    },
-    appBar: {
-        color: 'secondary',
-        zIndex: theme.zIndex.drawer + 1,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-    },
-    appBarShift: {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
     },
     menuButton: {
         marginRight: 36,
@@ -100,13 +80,7 @@ const useStyles = makeStyles((theme) => ({
 const Dashboard = () => {
     const classes = useStyles();
     const [open, setOpen] = React.useState(true);
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
-    const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+    
 
     const theme = React.useMemo(
         () =>
@@ -123,32 +97,15 @@ const Dashboard = () => {
                 <CssBaseline />
 
                 <Drawer />
-                
+
                 <main className={classes.content}>
                     <div className={classes.appBarSpacer} />
                     <Container maxWidth='lg' className={classes.container}>
-                        <Grid container spacing={6}>
-                            {/* Recent Deposits */}
-                            <Deposits />
-
-                            {/* Chart */}
-                            <Grid item xs={12} md={8} lg={6}>
-                                <Paper className={fixedHeightPaper}>
-                                    <Chart />
-                                </Paper>
-                            </Grid>
-
-                            {/* New Orders */}
-                            <Grid item xs={12} md={4} lg={6}>
-                                <Paper className={classes.paper}>
-                                    <Orders />
-                                </Paper>
-                            </Grid>
-                        </Grid>
+                        
                     </Container>
                 </main>
             </div>
         </ThemeProvider>
     );
-};
+}
 export default Dashboard;
