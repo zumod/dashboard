6e9dd5419b5f7e1a90424627e612be7fa66c8d1d
Sumod Sivadas <contactsumod@gmail.com>
2020-12-20 18:18:45 +0530

    Paper style added

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import clsx from 'clsx';
 
 const useStyles = makeStyles((theme) => ({
     depositContext: {
         flex: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        display: 'flex',
+        overflow: 'auto',
+        flexDirection: 'column',
     },
     fixedHeight: {
         height: 350,
