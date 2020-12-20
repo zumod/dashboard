e57c5f9a862cd3397f680e5c16c6a25deb551da4
Sumod Sivadas <contactsumod@gmail.com>
2020-12-20 18:45:01 +0530

    Bug fixed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Dashboard from './Components/Dashboard';
 import Settings from './Components/Settings';
 
 const App = () => {
     return (
         <div className='App'>
+            <BrowserRouter>
             <Switch>
                 <Route path='/' exact component={Dashboard} />
                 <Route path='/settings' component={Settings} />
             </Switch>
+            </BrowserRouter>
         </div>
     );
 };
